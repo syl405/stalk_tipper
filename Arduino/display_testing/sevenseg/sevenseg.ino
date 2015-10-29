8bf03f8b52566f46170933c4398ae745d2b9fdd3
Shien Yang Lee <syl405@nyu.edu>
2015-10-29 16:49:25 +0400

    Example code for Adafruit 7-segment display.

---
+++ Arduino/display_testing/sevenseg/sevenseg.ino
@@ -0,0 +1,48 @@
+/*************************************************** 
+  This is a library for our I2C LED Backpacks
+
+  Designed specifically to work with the Adafruit LED 7-Segment backpacks 
+  ----> http://www.adafruit.com/products/881
+  ----> http://www.adafruit.com/products/880
+  ----> http://www.adafruit.com/products/879
+  ----> http://www.adafruit.com/products/878
+
+  These displays use I2C to communicate, 2 pins are required to 
+  interface. There are multiple selectable I2C addresses. For backpacks
+  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
+  with 3 Address Select pins: 0x70 thru 0x77
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Enable one of these two #includes and comment out the other.
+// Conditional #include doesn't work due to Arduino IDE shenanigans.
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+//#include <TinyWireM.h> // Enable this line if using Adafruit Trinket, Gemma, etc.
+
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+void setup() {
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+  Serial.println("7 Segment Backpack Test");
+#endif
+  matrix.begin(0x70);
+  matrix.setBrightness(0);
+}
+
+void loop() {
+  // print a floating point 
+  matrix.print(12.34);
+  matrix.writeDisplay();
+  matrix.blinkRate(1);
+  delay(5000);
+}
